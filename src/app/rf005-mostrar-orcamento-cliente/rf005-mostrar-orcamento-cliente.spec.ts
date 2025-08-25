0a243c39fe5bfee59f1c16a220b58e794356e057
Jpdeger <joaopedro_deger@hotmail.com>
2025-08-25 16:02:30 -0300

    RF006 e RF007
    rf006 e rf007 já implementados na pagina RF005

--- src/app/rf005-mostrar-orcamento-cliente/rf005-mostrar-orcamento-cliente.spec.ts
+++ src/app/rf005-mostrar-orcamento-cliente/rf005-mostrar-orcamento-cliente.spec.ts
@@ -1,23 +1,29 @@
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Rf005MostrarOrcamentoCliente } from './rf005-mostrar-orcamento-cliente';
+
 
 describe('Rf005MostrarOrcamentoCliente', () => {
   let component: Rf005MostrarOrcamentoCliente;
   let fixture: ComponentFixture<Rf005MostrarOrcamentoCliente>;
 
   beforeEach(async () => {
+    
     await TestBed.configureTestingModule({
-      imports: [Rf005MostrarOrcamentoCliente]
+      imports: [Rf005MostrarOrcamentoCliente] 
     })
-    .compileComponents();
+    .compileComponents(); 
 
+    
     fixture = TestBed.createComponent(Rf005MostrarOrcamentoCliente);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); 
   });
 
+  
   it('should create', () => {
+   
     expect(component).toBeTruthy();
   });
 });
--- src/app/rf005-mostrar-orcamento-cliente/rf005-mostrar-orcamento-cliente.ts
+++ src/app/rf005-mostrar-orcamento-cliente/rf005-mostrar-orcamento-cliente.ts
@@ -1,26 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { Solicitacaoservice } from '../services/solicitacaoservice';
 import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rf005-mostrar-orcamento-cliente',
+  standalone: true, 
   imports: [DatePipe],
-  templateUrl: './rf005-mostrar-orcamento-cliente.html',
-  styleUrl: './rf005-mostrar-orcamento-cliente.css'
+  templateUrl: './rf005-mostrar-orcamento-cliente.html', 
+  styleUrls: ['./rf005-mostrar-orcamento-cliente.css'] 
 })
-export class Rf005MostrarOrcamentoCliente implements OnInit{
+export class Rf005MostrarOrcamentoCliente implements OnInit {
+ 
   solicitacoes: any[] = [];
+ 
+  motivoRejeicao: string = "";
 
-  constructor(private solicitacaoService: Solicitacaoservice) {};
-  ngOnInit(): void {}
-
-      rf005showSolicitacoes(){
-    this.solicitacoes = this.solicitacaoService.getSolicitacoes();
-    /*
-      for(let i in this.solicitacoes){
-      console.log(`${this.solicitacoes[i].descricaoEquipamento} ${this.solicitacoes[i].descricaoDefeito}`)
-      }
-    */
-  }
   
+  constructor(
+    private solicitacaoService: Solicitacaoservice, 
+    private router: Router 
+  ) {}
+
+
+  ngOnInit(): void {
+   
+    this.rf005showSolicitacoes();
+  }
+
+  
+  rf005showSolicitacoes() {
+    this.solicitacoes = this.solicitacaoService.getSolicitacoes();
+  }
+
+ 
+  aprovarServico(solicitacao: any): void {
+    
+
+    alert(`Serviço Aprovado no Valor R$ ${solicitacao.valorOrcado}`);
+    
+  
+    solicitacao.status = 'APROVADA';
+   
+    this.router.navigate(['/rf003']);
+  }
+
+
+  rejeitarServico(solicitacao: any): void {
+
+    const motivo = prompt("Por favor, informe o motivo da rejeição:");
+ 
+    if (motivo && motivo.trim() !== '') {
+      
+      alert("Serviço Rejeitado!");
+      
+    
+      solicitacao.status = 'REJEITADA';
+      
+    
+      this.router.navigate(['/rf003']);
+
+    } else {
+   
+      alert("O motivo da rejeição é obrigatório.");
+    }
+  }
 }
