ddd32d27cd6be7caf39c3d20140e9bb2bca12ac3
Leagueofcorno <cauadias.henrique@gmail.com>
2025-08-26 02:38:26 -0300

    rf008-visualizar-servico

---
+++ src/app/rf008-visualizar-servico/rf008-visualizar-servico.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rf008-visualizar-servico',
+  imports: [],
+  templateUrl: './rf008-visualizar-servico.html',
+  styleUrl: './rf008-visualizar-servico.css'
+})
+export class RF008VisualizarServico {
+
+}
