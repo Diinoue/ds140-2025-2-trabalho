5cdc8dbd0d5bc2e360594a69d4d040a0250b99dc
VitorHigen <vitorhigen@ufpr.br>
2025-11-11 23:50:52 -0300

    asd

--- src/app/pages/crud-funcionario/crud-funcionario.ts
+++ src/app/pages/crud-funcionario/crud-funcionario.ts
@@ -3,6 +3,8 @@ import { Funcionarioservice } from '../../services/funcionarioservice';
 import { Funcionario } from '../../shared/models/funcionario.model';
 import { VisualizarFuncionario } from '../visualizar-funcionario/visualizar-funcionario';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Loginservice } from '../../services/loginservice';
+import { Usuario } from '../../shared/models/usuario.model';
 
 @Component({
   selector: 'app-crud-funcionario',
@@ -12,28 +14,46 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 export class CrudFuncionario implements OnInit{
   Funcionarios:Funcionario[]=[];
-  funcionarioLogin: number = 0;
+  funcionarioLogin: Usuario = new Usuario;
 constructor(
     private funcionarioService: Funcionarioservice,  
+    private loginService: Loginservice,
     private modalService: NgbModal,
   ) {}
 
 ngOnInit(): void {
-  this.Funcionarios = this.funcionarioService.listarTodos();
-  this.funcionarioLogin = this.funcionarioService.getLogin();
+  this.carregarFuncionarios();
+  const res = this.loginService.usuarioLogado;
+  if (res == null) console.log("nao logado");
+  else this.funcionarioLogin = res;
+  //TEM QUE SABER O USUARIO LOGADO AQUI
+}
+
+carregarFuncionarios(): void {
+  this.funcionarioService.listarTodos().subscribe({
+    next: (dados) => this.Funcionarios = dados,
+    error: () => alert('Erro ao carregar funcionários.')
+  });
 }
 
 deletarFuncionario(id: number): void {
   if(window.confirm("Você tem certeza? Os dados serão excluidos permanentemente."))
   {
-    if(this.funcionarioLogin != id) {
+
+    if(this.funcionarioLogin.id != id) {
       if(this.Funcionarios.length > 1) {
-      this.funcionarioService.remover(id);
-      this.Funcionarios = this.funcionarioService.listarTodos();
-      }
-      else alert("Existem apenas 1 funcionario, função indisponivel");
+      this.funcionarioService.remover(id).subscribe({
+        next:(res) => {
+          this.carregarFuncionarios();
+        },
+        error: () => {
+              alert('Falha ao excluir funcionário.');
+        },
+      });
+      } else alert("Existem apenas 1 funcionario, função indisponivel");
     }
     else alert("Você não pode se excluir");
+
   }
 }
 
