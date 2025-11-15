02162cae662455e5341f66b0ea07d614c454cd7e
Leagueofcorno <cauadias.henrique@gmail.com>
2025-11-14 23:38:58 -0300

    fun e cli on

--- back-end/reparo/src/main/java/br/net/razer/reparo/reparo/model/Cliente.java
+++ back-end/reparo/src/main/java/br/net/razer/reparo/reparo/model/Cliente.java
@@ -1,53 +1,50 @@
 package br.net.razer.reparo.reparo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+@AttributeOverrides({
+    @AttributeOverride(name = "id", column = @Column(name = "id_cli")),
+    @AttributeOverride(name = "nome", column = @Column(name = "nome_cli")),
+    @AttributeOverride(name = "senha", column = @Column(name = "senha_cli")),
+    @AttributeOverride(name = "rota", column = @Column(name = "rota_cli")),
+    @AttributeOverride(name = "email", column = @Column(name = "email_cli"))
+})
 public class Cliente extends Usuario {
 
+    @Column(name = "cpf_cli")
     private String cpf;
+
+    @Column(name = "telefone_cli")
     private String telefone;
+
+    @Column(name = "endereco_cli")
     private String endereco;
+
+    @Column(name = "cep_cli")
     private String cep;
 
-    public Cliente() {
-    }
+    public Cliente() {}
 
-    public Cliente(String nome, String senha, String rota, String email, Integer id, String cpf, String telefone, String endereco, String cep){
-        super(email, nome, senha, rota, id);
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.endereco = endereco;
-        this.cep = cep;
-    }
+  public Cliente(String email, String nome, String senha, String rota, Integer id,
+               String cpf, String telefone, String endereco, String cep) {
 
-    public String getCpf() {
-        return cpf;
-    }
+    super(email, nome, senha, rota, id);
+    this.cpf = cpf;
+    this.telefone = telefone;
+    this.endereco = endereco;
+    this.cep = cep;
+}
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 
-    public String getTelefone() {
-        return telefone;
-    }
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    
-    public String getCEP() {
-        return cep;
-    }
-
-    public void setCEP(String cep) {
-        this.cep = cep;
-    }
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
 }
--- back-end/reparo/src/main/java/br/net/razer/reparo/reparo/repo/FuncionarioRepository.java
+++ back-end/reparo/src/main/java/br/net/razer/reparo/reparo/repo/FuncionarioRepository.java
@@ -1,4 +1,4 @@
-package br.net.razer.reparo.reparo.repositorio;
+package br.net.razer.reparo.reparo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
